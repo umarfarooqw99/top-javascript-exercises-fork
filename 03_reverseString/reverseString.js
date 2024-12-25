ccbe7aa1124ef5bb70064d889ac9694115919ee5
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2024-12-25 19:33:51 +0500

    Finish 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let charArray = inputString.split("");
+    let reversedArray = [];
+    for(let i=0; i<charArray.length; i++) {
+        reversedArray.unshift(charArray[i]);
+    }
+    let reverseString = reversedArray.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
