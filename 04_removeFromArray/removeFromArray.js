73496dcce49b8fcf22fe930851a67f92fc12ca63
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2024-12-25 22:51:04 +0500

    finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let argList = [];
+    for(let i=1; i<arguments.length; i++) {
+        argList.push(arguments[i]);
+    }
+    let filteredArray = arr.filter(x => !argList.includes(x));
+    return filteredArray;
 };
 
 // Do not edit below this line
