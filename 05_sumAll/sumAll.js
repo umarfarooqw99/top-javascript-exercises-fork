75e6af3369e1fefe7cbafc9204a05d1e13c642e4
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2024-12-25 23:06:39 +0500

    finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof a != "number" || typeof b != "number") return "ERROR";
+    if(a<0 || b<0) return "ERROR";
+    if(Math.floor(a) != a || Math.floor(b) != b) return "ERROR";
 
+    let numberOfTerms = Math.abs(a-b) + 1;
+    let sum = numberOfTerms * (a + b) / 2; //arithmetic sum formula
+    return sum;
 };
 
 // Do not edit below this line
