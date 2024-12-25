f3ebca8fb3d7823db1afc4439ab5f0e9250fe9b4
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2024-12-25 23:33:04 +0500

    finish 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (5/9) * (tempFahrenheit - 32);
+  tempCelsius = Math.round(tempCelsius * 10) / 10;
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = (9/5) * tempCelsius + 32;
+  tempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
