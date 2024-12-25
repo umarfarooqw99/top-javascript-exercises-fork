3937f0eeb0c077dbc281524cc8ff77fe0582f02c
Umar Farooq <136256966+umarfarooqw99@users.noreply.github.com>
2024-12-25 23:50:14 +0500

    finish 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(let i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for(let i=0; i<arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let power = 1;
+  for(let i=0; i<exponent; i++) {
+    power *= base;
+  }
+  return power;	
 };
 
-const factorial = function() {
+const factorial = function(number) {
+  if(number == 0 || number == 1) return 1;
+  let factorial = 1;
+  while (number > 1) {
+    factorial *= number;
+    number--;
+  }
+  return factorial;
 	
 };
 
